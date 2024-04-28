7ff3e381616b6b27013ea53ba4340f59ec70031a
venomkfldmdf12321 <134198188+venomkfldmdf12321@users.noreply.github.com>
2024-04-28 16:20:01 +0530

    Update ad.js

--- app/component/ad.js
+++ app/component/ad.js
@@ -1,5 +1,4 @@
 'use client'
-
 import React, { useEffect, useState } from 'react';
 import LazyLoad from 'react-lazyload';
 import he from 'he'; // Import the he library for HTML entity decoding
@@ -14,205 +13,103 @@ const ContentWithAds = () => {
             .then(html => {
                 // Define your ad tags
                 const adTags = [
-                    `<div>
-                        <!-- Lazy-loaded ad tag 1 -->
-                        <LazyLoad height={200} offset={100}>
-                            <div>
-
-
-                            <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-                            <script>
-                              window.googletag = window.googletag || {cmd: []};
-                              googletag.cmd.push(function() {
-                                googletag.defineSlot('/22515155853/tv9_hindi_UP_RJ_32', [300, 250], 'div-gpt-ad-23126880471').addService(googletag.pubads());
-                                googletag.pubads().enableSingleRequest();
-                                googletag.enableServices();
-                              });
-                            </script>
-                            <div class="ad">
-                            <div id='div-gpt-ad-23126880471' style='min-width: 300px; min-height: 250px;'>
-                                <script>
-                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-23126880471'); });
-                                </script>
-                            </div>
-                        </div>
-
-                            
-                            </div>
-                        </LazyLoad>
-                    </div>`,
-
-                    `<div>
-                        <!-- Lazy-loaded ad tag 2 -->
-                        <LazyLoad height={200} offset={100}>
-                            <div>
-
-
-                            <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-                            <script>
-                              window.googletag = window.googletag || {cmd: []};
-                              googletag.cmd.push(function() {
-                                googletag.defineSlot('/22515155853/tv9_hindi_UP_RJ_33', [300, 250], 'div-gpt-ad-23126881002').addService(googletag.pubads());
-                                googletag.pubads().enableSingleRequest();
-                                googletag.enableServices();
-                              });
-                            </script>
-                            <div class="ad"><!-- /23126881002 -->
-                            <div id='div-gpt-ad-23126881002' style='min-width: 300px; min-height: 250px;'>
-                                <script>
-                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-23126881002'); });
-                                </script>
-                            </div>
-                        </div>
-
-
-                            </div>
-                        </LazyLoad>
-                    </div>`,
-
-                    `<div>
-                        <!-- Lazy-loaded ad tag 3 -->
-                        <LazyLoad height={200} offset={100}>
-                            <div>
-
-
-                            <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-                            <script>
-                              window.googletag = window.googletag || {cmd: []};
-                              googletag.cmd.push(function() {
-                                googletag.defineSlot('/22515155853/tv9_hindi_UP_RJ_37', [300, 250], 'div-gpt-ad-23126882949').addService(googletag.pubads());
-                                googletag.pubads().enableSingleRequest();
-                                googletag.enableServices();
-                              });
-                            </script>
-                            <div class="ad"><!-- /23126882949 -->
-                            <div id='div-gpt-ad-23126882949' style='min-width: 300px; min-height: 250px;'>
-                                <script>
-                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-23126882949'); });
-                                </script>
-                            </div>
-                        </div>    
-                        
-                        
-
-                        </div>
-                        </LazyLoad>
-                    </div>`,
-
-                    `<div>
-                        <!-- Lazy-loaded ad tag 4 -->
-                        <LazyLoad height={200} offset={100}>
-                            <div>
-
-
-                            <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-                            <script>
-                              window.googletag = window.googletag || {cmd: []};
-                              googletag.cmd.push(function() {
-                                googletag.defineSlot('/22515155853/tv9_hindi_UP_RJ_48', [300, 250], 'div-gpt-ad-23126892420').addService(googletag.pubads());
-                                googletag.pubads().enableSingleRequest();
-                                googletag.enableServices();
-                              });
-                            </script>
-                            <div class="ad"><!-- /23126892420 -->
-                            <div id='div-gpt-ad-23126892420' style='min-width: 300px; min-height: 250px;'>
-                                <script>
-                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-23126892420'); });
-                                </script>
-                            </div>
-                        </div>
-
-
-
-                        </div>
-                        </LazyLoad>
-                    </div>`,
-
-                    `<div>
-                        <!-- Lazy-loaded ad tag 5 -->
-                        <LazyLoad height={200} offset={100}>
-                            <div>
-
-
-                            <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-                            <script>
-                              window.googletag = window.googletag || {cmd: []};
-                              googletag.cmd.push(function() {
-                                googletag.defineSlot('/22515155853/tv9_hindi_UP_RJ_14', [300, 250], 'div-gpt-ad-23127116740').addService(googletag.pubads());
-                                googletag.pubads().enableSingleRequest();
-                                googletag.enableServices();
-                              });
-                            </script>
-                            <div class="ad"><!-- /23127116743 -->
-            <div id='div-gpt-ad-23127116743' style='min-width: 300px; min-height: 250px;'>
-                <script>
-                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-23127116743'); });
-                </script>
-            </div>
-        </div>
-
-
-        </div>
-                        </LazyLoad>
-                    </div>`,
-
-                    `<div>
-                        <!-- Lazy-loaded ad tag 6 -->
-                        <LazyLoad height={200} offset={100}>
-                            <div>
-
-
-                            <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-                            <script>
-                              window.googletag = window.googletag || {cmd: []};
-                              googletag.cmd.push(function() {
-                                googletag.defineSlot('/22515155853/tv9_hindi_UP_RJ_19', [300, 250], 'div-gpt-ad-23127116743').addService(googletag.pubads());
-                                googletag.pubads().enableSingleRequest();
-                                googletag.enableServices();
-                              });
-                            </script>
-                            <div class="ad"><!-- /23127116746 -->
-                            <div id='div-gpt-ad-23127116746' style='min-width: 300px; min-height: 250px;'>
-                                <script>
-                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-23127116746'); });
-                                </script>
-                            </div>
-                        </div>                
-
-
-                        </div>
-                        </LazyLoad>
-                    </div>`,
-                    `<div>
-                        <!-- Lazy-loaded ad tag 6 -->
-                        <LazyLoad height={200} offset={1000}>
-                        <img src="https://picsum.photos/200"/>
-                        </LazyLoad>
-                    </div>`,
-                    `<div>
-                    <!-- Lazy-loaded ad tag 6 -->
-                    <LazyLoad height={200} offset={1000}>
-                    <img src="https://picsum.photos/201"/>
-                    </LazyLoad>
-                </div>`,
-                    `<div>
-                <!-- Lazy-loaded ad tag 6 -->
-                <LazyLoad height={200} offset={1000}>
-                <img src="https://picsum.photos/203"/>
-                </LazyLoad>
-            </div>`,
-                    `<div>
-            <!-- Lazy-loaded ad tag 6 -->
-            <LazyLoad height={200} offset={1000}>
-            <img src="https://picsum.photos/204"/>
-            </LazyLoad>
-        </div>`,
+                    {
+                        slotId: 'div-gpt-ad-23129971914',
+                        adUnitPath: '/22515155853/Hangama_BJP_1'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971917',
+                        adUnitPath: '/22515155853/Hangama_BJP_13'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971920',
+                        adUnitPath: '/22515155853/Hangama_BJP_16'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971923',
+                        adUnitPath: '/22515155853/Hangama_BJP_20'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971926',
+                        adUnitPath: '/22515155853/Hangama_BJP_23'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971929',
+                        adUnitPath: '/22515155853/Hangama_BJP_26'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971932',
+                        adUnitPath: '/22515155853/Hangama_BJP_3'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971935',
+                        adUnitPath: '/22515155853/Hangama_BJP_5'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129971938',
+                        adUnitPath: '/22515155853/Hangama_BJP_8'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972355',
+                        adUnitPath: '/22515155853/Hangama_BJP_10'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972358',
+                        adUnitPath: '/22515155853/Hangama_BJP_11'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972361',
+                        adUnitPath: '/22515155853/Hangama_BJP_12'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972364',
+                        adUnitPath: '/22515155853/Hangama_BJP_14'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972357',
+                        adUnitPath: '/22515155853/Hangama_BJP_15'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972370',
+                        adUnitPath: '/22515155853/Hangama_BJP_17'
+                    },
+                    {
+                        slotId: 'div-gpt-ad-23129972373',
+                        adUnitPath: '/22515155853/Hangama_BJP_18'
+                    },
+                    // Add more ad tags here as needed
                 ];
 
-                // Replace placeholders with actual ad tags
+                // Replace placeholders with ad slots
                 let updatedHtml = html;
                 adTags.forEach((adTag, index) => {
-                    updatedHtml = updatedHtml.replace(`<!-- adTag${index + 1}Placeholder -->`, adTag);
+                    const { slotId } = adTag;
+                    const placeholderRegex = new RegExp(`<!-- adTag${index + 1}Placeholder -->`, 'g');
+                    updatedHtml = updatedHtml.replace(placeholderRegex, `<LazyLoad><div id="${slotId}"></div></LazyLoad>`);
+                });
+
+                // Generate ad tag elements dynamically
+                adTags.forEach(adTag => {
+                    const { slotId, adUnitPath } = adTag;
+
+                    // Create script element for GPT
+                    const gptScript = document.createElement('script');
+                    gptScript.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
+                    gptScript.async = true;
+                    document.head.appendChild(gptScript);
+
+                    // Create script element for ad tag
+                    const adScript = document.createElement('script');
+                    adScript.innerHTML = `
+                        window.googletag = window.googletag || {cmd: []};
+                        googletag.cmd.push(function() {
+                          googletag.defineSlot('${adUnitPath}', [300, 250], '${slotId}').addService(googletag.pubads());
+                          googletag.pubads().enableSingleRequest();
+                          googletag.enableServices();
+                        });
+                        googletag.cmd.push(function() { googletag.display('${slotId}'); });
+                    `;
+                    document.body.appendChild(adScript);
                 });
 
                 setContent(updatedHtml);
@@ -225,4 +122,3 @@ const ContentWithAds = () => {
 };
 
 export default ContentWithAds;
-
